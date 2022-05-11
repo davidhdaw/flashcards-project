aa6aa1a1e762b63bf163b398083215b9d202cc56
David Daw <daviddaw@gmail.com>
2022-05-10 19:53:08 -0700

    Add Round class that passes all tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -18,44 +18,54 @@ describe('Round', function() {
   it('should be a function', () => {
     expect(Round).to.be.a('function');
   });
+
   it('should be an instance of Deck', () => {
     expect(round).to.be.an.instanceof(Round);
   });
+
   it('should take in a Deck', () => {
-    expect(round.deck).to.equal(deck);
+    expect(round.deck).to.equal(deck.cards);
   });
+
   it('should start with the first card in the deck', () => {
     expect(round.returnCurrentCard()).to.equal(round.deck[0]);
   });
-  it('should create a new Turn isntance when a guess is made', () => {
+
+  it('should create a new Turn instance when a guess is made', () => {
     round.takeTurn('dogs');
     expect(round.turn.returnGuess()).to.equal('dogs')
   });
+
   it('should update a turn count regardless of the guess', () => {
     round.takeTurn('dogs');
     round.takeTurn('gallbladder');
     expect(round.turnCount).to.equal(2);
   });
+
   it('should change current card after each turn', () => {
     round.takeTurn('dogs');
     round.takeTurn('gallbladder');
     expect(round.returnCurrentCard()).to.equal(round.deck[2]);
   });
+
   it('should evaluate guesses and store incorrect answers in incorrectGuesses', () => {
     round.takeTurn('dogs');
     round.takeTurn('gallbladder');
-    expect(round.incorrectGuesses).to.equal([1]);
+    expect(round.incorrectGuesses).to.eql([1]);
   });
+
   it('should give feedback if the answer is correct or incorrect.', () => {
     expect(round.takeTurn('dogs')).to.equal('incorrect!');
-    expect(round.takeTurn('gallblader')).to.equal('correct!');
+    expect(round.takeTurn('gallbladder')).to.equal('correct!');
   });
+
   it('should be able to calculate the percentage of correct answers', () => {
     round.takeTurn('dogs');
     round.takeTurn('gallbladder');
     round.takeTurn('Fitzgerald');
     expect(round.calculatePercentCorrect()).to.equal(2/3);
   });
+
   it('should be able to print out a message at the end of a round', () => {
     round.takeTurn('dogs');
     round.takeTurn('gallbladder');

da87b52ffce9c51b80e125f0a0676186cce94756
David Daw <daviddaw@gmail.com>
2022-05-12 15:03:05 -0700

    Add game startup functions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-// This is where your project starts.
+const Card = require('./src/Card');
+const Deck = require('./src/Deck');
+const Game = require('./src/Game');
+const Round = require('./src/Round');
+const data = require('./src/data');
+const prototypeQuestions = data.prototypeData;
 
-console.log('Your project is running...'); 
+console.log('Your project is running...');
+const newGame = new Game();
+newGame.start()

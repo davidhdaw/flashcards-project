4a26b1c2b94b77916a8a584af67b54c3d86f19e6
David Daw <daviddaw@gmail.com>
2022-05-12 14:59:53 -0700

    Add Game class that passes all tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,33 +3,28 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Game = require('../src/Game');
+const Round = require('../src/Round');
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 describe('Game', function() {
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  const fullDeck = new Deck([card1, card2, card3]);
-  const shortDeck = new Deck([card1, card2]);
-  const cardArray = [card1, card2, card3];
-  const newGame = newGame();
-
+  const newGame = new Game();
+  newGame.start()
   it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
   it('should be an instance of Game', () => {
     expect(newGame).to.be.an.instanceof(Game);
   });
-  it('should create cards on start' () => {
-    newGame.start()
-    expect(newGame.cardsArry).to.be.a('array');
+  it('should create cards on start', () => {
+
+    expect(newGame.cards.length).to.equal(prototypeQuestions.length);
   })
-  it('should create a deck on start' () => {
-    newGame.start()
-    expect(newGame.deck).to.be.an.instance(Deck);
+  it('should create a deck on start', () => {
+    expect(newGame.deck).to.be.an.instanceof(Deck);
   })
-  it('should create a round on start' () => {
-    newGame.start()
-    expect(newGame.currentRound).to.be.an.instance(Round);
+  it('should create a round on start', () => {
+    expect(newGame.currentRound).to.be.an.instanceof(Round);
   })
   //should invoke printMessage
   //should invokePrintQuestion
